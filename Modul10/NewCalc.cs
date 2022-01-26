ecc25e533d785d18fca190d66eafe8e8e2770fea
halkman <halk-man@mail.ru>
2022-01-26 16:46:38 +0500

    Добавьте файлы проекта.

---
+++ Modul10/NewCalc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugWork1
+{
+    public class CalculateP : ICalc<double, double>
+    {
+        ILogger Logger { get; }
+        public CalculateP(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public double Calc(double x, double y)
+        {
+            Logger.Event("  Начинается подсчет суммы чисел .");
+            Thread.Sleep(2000);
+            return x + y;
+
+        }
+    }
+    public class CalculateM : ICalc<double, double>
+    {
+        ILogger Logger { get; }
+        public CalculateM(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public double Calc(double x, double y)
+        {
+            Logger.Event("  Начинается подсчет суммы чисел .");
+            Thread.Sleep(2000);
+            return x - y;
+
+        }
+    }
+}
---
+++ Modul10/Program.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+using System.Collections.Generic;
+namespace DebugWork1
+{
+
+
+    class Program
+    {
+        static ILogger logger { get; set; }
+        public static void Main(string[] args)
+        {
+            logger = new Logger();
+            CalculateP calcp = new CalculateP(logger);
+            CalculateM calcm = new CalculateM(logger);
+            Console.WriteLine("Добро пожаловать в примитивный калькулятор!");
+            string choice = null;
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Введите первое число : ");
+                    double x = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введите второе число : ");
+                    double y = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Выберите операцию + или - :");
+                    choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "+":
+                            double resp = calcp.Calc(x, y);
+                            Console.WriteLine($"Результатом сложения является чиcло : {resp}");
+                            break;
+                        case "-":
+                            double resm = calcm.Calc(x, y);
+                            Console.WriteLine($"Результатом вычитания является чиcло : {resm}");
+                            break;
+                        default:
+                            Console.WriteLine("Давайте еще раз введем цифры и всётаки выберем операцию + или - !!!");
+                            break;
+                    }
+                }
+                while (choice != "+" && choice != "-");
+            }
+            catch (FormatException ex) { logger.Error(ex.Message); }
+            catch (Exception e) { logger.Error(e.Message); }
+        }
+    }
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+    public interface ICalc<T, Z>
+    {
+        public double Calc(T x, T y);
+    }
+}
